de5cbe12b92a607114fa39551c46402a688f58fb
Anbarasan <anbarasanj24@gmail.com>
2021-06-21 08:26:11 +0530

    "Added Comment Details Task"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import CommentDetails from './CommentDetails';
-import Segment from './Segment';
+// import CommentDetails from './CommentDetails/CommentDetails';
+// import Segment from './CommentDetails/Segment';
 
 const App = () => {
     return (
-        <Segment>
-            <div className="ui container comments">
-                <CommentDetails author="Anbu" time="Today" comment="Have a nice day!" />
-            </div>
-        </Segment>
+
+        <h1>Hello</h1>
+
+
+
+
+
+
+
+
+
+
+        // Task 1 -> Comment Details
+
+        // <Segment>
+        //     <div className="ui container comments">
+        //         <CommentDetails author="Anbu" time="Today" comment="Have a nice day!" />
+        //     </div>
+        // </Segment>
     )
 }
 
