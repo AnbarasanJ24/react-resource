fda968f819955ef557f90814bc06b5a2aaca5000
Anbarasan <anbarasanj24@gmail.com>
2021-06-21 09:24:25 +0530

    Added Seasons Task Details

---
+++ src/Seasons/SeasonsDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonsDisplay = () => {
+    return (
+        <div>Season</div>
+    );
+};
+
+export default SeasonsDisplay;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import SeasonsDisplay from './Seasons/SeasonsDisplay';
 // import CommentDetails from './CommentDetails/CommentDetails';
 // import Segment from './CommentDetails/Segment';
 
-const App = () => {
-    return (
+class App extends React.Component {
+    constructor() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        )
 
-        <h1>Hello</h1>
+    }
 
-
-
-
-
-
-
-
-
-
-        // Task 1 -> Comment Details
-
-        // <Segment>
-        //     <div className="ui container comments">
-        //         <CommentDetails author="Anbu" time="Today" comment="Have a nice day!" />
-        //     </div>
-        // </Segment>
-    )
+    render() {
+        return (
+            <SeasonsDisplay></SeasonsDisplay >
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+// Task 1 -> Comment Details
+
+// <Segment>
+//     <div className="ui container comments">
+//         <CommentDetails author="Anbu" time="Today" comment="Have a nice day!" />
+//     </div>
+// </Segment>
