20978e76f5e42b48c6cbdbff5a4ae999dbcd1a05
Anbarasan <anbarasanj24@gmail.com>
2021-06-24 23:18:13 +0530

    Clen up for next task

--- src/index.js
+++ src/index.js
@@ -1,52 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import SeasonsDisplay from './Seasons/SeasonsDisplay';
-import Spinner from './Seasons/Spinner';
-// import CommentDetails from './CommentDetails/CommentDetails';
-// import Segment from './CommentDetails/Segment';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-
-    state = {
-        latitude: null,
-        errorMessage: ''
-    }
-
-    componentDidMount() {
-        console.log("Component Did Mount");
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ latitude: position.coords.latitude }),
-            err => this.setState({ errorMessage: err.message })
-        )
-    }
-
-    componentDidUpdate() {
-        console.log("Component Update Mount")
-    }
-
-    renderBody() {
-
-        if (this.state.errorMessage && !this.state.latitude) {
-            return <h1>{this.state.errorMessage}</h1>;
-        }
-        if (!this.state.errorMessage && this.state.latitude) {
-            return <SeasonsDisplay lat={this.state.latitude} />
-        }
-        return <Spinner />
-    }
-
-
     render() {
         return (
-            <div className="border red">{this.renderBody()}</div>
+            <div>Test</div>
         );
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
 
 // Task 1 -> Comment Details
 
@@ -55,3 +18,39 @@ ReactDOM.render(<App />, document.getElementById('root'));
 //         <CommentDetails author="Anbu" time="Today" comment="Have a nice day!" />
 //     </div>
 // </Segment>
+
+// Task 2 -> Seasons Display
+// state = {
+//     latitude: null,
+//     errorMessage: ''
+// }
+
+// componentDidMount() {
+//     console.log("Component Did Mount");
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => this.setState({ latitude: position.coords.latitude }),
+//         err => this.setState({ errorMessage: err.message })
+//     )
+// }
+
+// componentDidUpdate() {
+//     console.log("Component Update Mount")
+// }
+
+// renderBody() {
+
+//     if (this.state.errorMessage && !this.state.latitude) {
+//         return <h1>{this.state.errorMessage}</h1>;
+//     }
+//     if (!this.state.errorMessage && this.state.latitude) {
+//         return <SeasonsDisplay lat={this.state.latitude} />
+//     }
+//     return <Spinner />
+// }
+
+
+// render() {
+//     return (
+//         <div className="border red">{this.renderBody()}</div>
+//     );
+// }
